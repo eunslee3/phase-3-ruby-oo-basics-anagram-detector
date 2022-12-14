ad3ff75e3a24a4e0b06ac39fb5f2c1b5d63a92b6
Samuel <lee.eunseong7856@gmail.com>
2022-12-14 10:08:48 -0600

    commit tree dcd7246b20c9102576d5f99b651d879b777d22bc

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+    attr_reader :listen, :match
+
+    def initialize(listen)
+        @listen = listen
+    end
+
+    def match
+        @match = match
+        @listen.filter {|word| word == @match}
+    end
+end
